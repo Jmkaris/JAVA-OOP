7b6a4b486054f99ef17ec2815e5fd13ca08984ce
Jmkaris <mugwimikariuki101@gmail.com>
2026-02-03 11:53:06 +0300

    Refactor Main class to streamline student initialization and enhance greeting method

--- Main.java
+++ Main.java
@@ -1,18 +1,14 @@
 public class Main{
     public static void main(String[] args){
         
-        Student [] students = new Student[5];
-        
-        
+        // Student [] students = new Student[5];
         Student james = new Student();
 
-        james.name = "James Kariuki";
-        james.gpa = 3.5f;
-        james.grade = 56;
+        james.greeting();
 
-        System.out.println(james.grade);
-        System.out.println(james.name);
-        System.out.println(james.gpa);
+        // System.out.println(james.grade);
+        // System.out.println(james.name);
+        // System.out.println(james.gpa);
     }
 
     static class Student{
@@ -20,5 +16,15 @@ public class Main{
         String name;
         float gpa;
 
+        Student(){
+            this.name = "James Kariuki";
+            this.gpa = 3.5f;
+            this.grade = 56;
+        }
+
+        void greeting(){
+            System.out.println("My name is " + name);
+        }
+       
     }
 }
