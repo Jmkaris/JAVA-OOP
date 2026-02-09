a8ea24011bdb94e33610bcc6ec018ebf7f363525
Jmkaris <mugwimikariuki101@gmail.com>
2026-02-09 12:56:24 +0300

    Add Car class with drive and stop methods; update Main to utilize Car functionality

---
+++ Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    String make = "Toyota";
+    String company = "Mj";
+    int year = 2025;
+    double price = 500000;
+
+    void drive(){
+        System.out.println("You drive this car");
+    }
+    void stop(){
+        System.out.println("You stop when uou want to ");
+    }
+}
--- Main.java
+++ Main.java
@@ -1,30 +1,8 @@
 public class Main{
     public static void main(String[] args){
-        
-        // Student [] students = new Student[5];
-        Student james = new Student();
-
-        james.greeting();
-
-        // System.out.println(james.grade);
-        // System.out.println(james.name);
-        // System.out.println(james.gpa);
-    }
-
-    static class Student{
-        int grade;
-        String name;
-        float gpa;
-
-        Student(){
-            this.name = "James Kariuki";
-            this.gpa = 3.5f;
-            this.grade = 56;
-        }
-
-        void greeting(){
-            System.out.println("My name is " + name);
-        }
-       
+        Car myCar = new Car();
+            // System.out.println(myCar.make);
+            // System.out.println(myCar.year);
+        myCar.drive();
     }
 }
