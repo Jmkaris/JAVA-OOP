ae6950baa7f9e349f643b486740f937cf15873fc
Jmkaris <mugwimikariuki101@gmail.com>
2026-02-12 13:35:00 +0300

    Update Methods class to include parameters in hello method for personalized greeting

--- Methods.java
+++ Methods.java
@@ -1,10 +1,18 @@
 // It is a block of code that is executed when called upon
 public class Methods {
     public static void main(String[] args){
-        hello();
+        
+        String name;
+        int age; 
+        name = "Karis";
+        age = 22;
+
+
+        hello(name, age);
     }
-    
-    static void hello(){
-        System.out.println("hello Karis");
+
+    static void hello(String name, int age){
+        System.out.println("hello " + name + " and I am " + age + "years old");
     }
 }
+
