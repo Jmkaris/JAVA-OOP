aa1faced6052f782da07cebbe0e49a5fc7ef3e53
Jmkaris <mugwimikariuki101@gmail.com>
2026-02-12 13:22:17 +0300

    Add Methods class with main and hello methods for greeting functionality

---
+++ Methods.java
@@ -0,0 +1,10 @@
+// It is a block of code that is executed when called upon
+public class Methods {
+    public static void main(String[] args){
+        hello();
+    }
+    
+    static void hello(){
+        System.out.println("hello Karis");
+    }
+}
