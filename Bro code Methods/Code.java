5bcd68b1f9992daded1c5367eefbebbc8a270f77
Jmkaris <mugwimikariuki101@gmail.com>
2026-03-02 08:47:46 +0300

    Update hello method to include age parameter for personalized greeting

--- Bro code Methods/Code.java
+++ Bro code Methods/Code.java
@@ -3,10 +3,12 @@ public class Code {
     public static void main(String[] args) {
 
         String name = "Karis";
-        hello(name);
+        int number = 4;
+        hello(name, number);
+
     }
 
-    static void hello(String name) {
-        System.out.println("Hello, " + name + "!");
+    static void hello(String name, int number) {
+        System.out.println("Hello, " + name + "! You are " + number + " years old.");
     }
 }
