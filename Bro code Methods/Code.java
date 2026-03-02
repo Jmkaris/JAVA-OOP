6cf0f00873e16932a654211a9e245f1e4363b117
Jmkaris <mugwimikariuki101@gmail.com>
2026-03-02 08:46:21 +0300

    Add Code class with main and hello methods for greeting functionality

---
+++ Bro code Methods/Code.java
@@ -0,0 +1,12 @@
+public class Code {
+
+    public static void main(String[] args) {
+
+        String name = "Karis";
+        hello(name);
+    }
+
+    static void hello(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+}
